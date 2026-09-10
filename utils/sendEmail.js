f924abc53a5b2730079893254d72f460429d8318
actionarun <actionofak@gmail.com>
2026-09-10 11:11:04 +0530

    mail bug fix

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,12 +2,15 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: process.env.EMAIL_PORT,
-  secure: false,
+  port: 465,
+  secure: true, // SSL - more reliable than port 587 from cloud hosts like Render
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 20000,
+  greetingTimeout: 20000,
+  socketTimeout: 20000,
 });
 
 const sendEmail = async ({ to, subject, html }) => {
